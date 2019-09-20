ae5b5565b023f2cd2ff55086939c3ee450be2305
cemkiy <cemekiy@gmail.com>
2019-09-21 01:14:35 +0300

    skip

--- Player.gd
+++ Player.gd
@@ -18,37 +18,55 @@ func _physics_process(delta):
 		velocity.x = 0
 		run()
 		
-		
 	if Input.is_action_pressed("ui_up") and is_on_floor():
 		velocity.y = SKIP_POWER
 	
+	if !is_on_floor() and velocity.y < -0.2:
+		skip()
+	
 	velocity.y += GRAVITY
-		
 	velocity = move_and_slide(velocity, FLOOR)
 	
 func right():
 	$RightAnimatedSprite.visible = true
 	$LeftAnimatedSprite.visible = false
 	$WalkingAnimatedSprite.visible = false
+	$SkipAnimatedSprite.visible = false
 	
 	$RightCollisionShape2D.disabled = false
 	$LeftCollisionShape2D.disabled = true
 	$WalkingCollisionShape2D.disabled = true
+	$SkipCollisionShape2D.disabled = true
 	
 func left():
 	$LeftAnimatedSprite.visible = true
 	$RightAnimatedSprite.visible = false
 	$WalkingAnimatedSprite.visible = false
+	$SkipAnimatedSprite.visible = false
 	
 	$LeftCollisionShape2D.disabled = false
 	$RightCollisionShape2D.disabled = true
 	$WalkingCollisionShape2D.disabled = true
+	$SkipCollisionShape2D.disabled = true
+	
+func skip():
+	$SkipAnimatedSprite.visible = true
+	$WalkingAnimatedSprite.visible = false
+	$LeftAnimatedSprite.visible = false
+	$RightAnimatedSprite.visible = false
+	
+	$SkipCollisionShape2D.disabled = false
+	$WalkingCollisionShape2D.disabled = true
+	$LeftCollisionShape2D.disabled = true
+	$RightCollisionShape2D.disabled = true
 	
 func run():
 	$WalkingAnimatedSprite.visible = true
 	$LeftAnimatedSprite.visible = false
 	$RightAnimatedSprite.visible = false
+	$SkipAnimatedSprite.visible = false
 	
 	$WalkingCollisionShape2D.disabled = false
 	$LeftCollisionShape2D.disabled = true
 	$RightCollisionShape2D.disabled = true
+	$SkipCollisionShape2D.disabled = true
