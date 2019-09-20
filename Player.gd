c327dc0a226cf01d3eb6e4ae9cdca3074d510596
cemkiy <cemekiy@gmail.com>
2019-09-21 00:29:21 +0300

    right-left animations

---
+++ Player.gd
@@ -0,0 +1,54 @@
+extends KinematicBody2D
+
+var velocity = Vector2()
+
+const SPEED = 60
+const GRAVITY = 5
+const SKIP_POWER = -120
+const FLOOR = Vector2(0, -1)
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+		right()
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+		left()
+	else:
+		velocity.x = 0
+		run()
+		
+		
+	if Input.is_action_pressed("ui_up") and is_on_floor():
+		velocity.y = SKIP_POWER
+	
+	velocity.y += GRAVITY
+		
+	velocity = move_and_slide(velocity, FLOOR)
+	
+func right():
+	$RightAnimatedSprite.visible = true
+	$LeftAnimatedSprite.visible = false
+	$WalkingAnimatedSprite.visible = false
+	
+	$RightCollisionShape2D.disabled = false
+	$LeftCollisionShape2D.disabled = true
+	$WalkingCollisionShape2D.disabled = true
+	
+func left():
+	$LeftAnimatedSprite.visible = true
+	$RightAnimatedSprite.visible = false
+	$WalkingAnimatedSprite.visible = false
+	
+	$LeftCollisionShape2D.disabled = false
+	$RightCollisionShape2D.disabled = true
+	$WalkingCollisionShape2D.disabled = true
+	
+func run():
+	$WalkingAnimatedSprite.visible = true
+	$LeftAnimatedSprite.visible = false
+	$RightAnimatedSprite.visible = false
+	
+	$WalkingCollisionShape2D.disabled = false
+	$LeftCollisionShape2D.disabled = true
+	$RightCollisionShape2D.disabled = true
